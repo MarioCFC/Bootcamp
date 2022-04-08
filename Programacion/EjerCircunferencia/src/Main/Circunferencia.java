ebff224a3526b5ad7203de5053780a4fca48691b
Mario Cascudo <mariocscdclase@gmail.com>
2022-04-08 12:47:28 +0200

    Empezamos programacion

---
+++ Programacion/EjerCircunferencia/src/Main/Circunferencia.java
@@ -0,0 +1,30 @@
+package Main;
+
+import java.util.logging.Logger;
+
+public class Circunferencia {
+
+	/*
+	 * EJERCICIO: Dado el radio, (15), calcular el área del círculo y la longitud de
+	 * la circunferencia.
+	 */
+
+	private static Logger logOut = Logger.getLogger("MyLog");
+
+	public static void main(String[] args) {
+		double radio = 15;
+		logOut.info("El area de la circunferencia de radio " + radio + " es: " + calcularArea(radio));
+
+		logOut.info("La longitud de la circunferencia de radio " + radio + " es: " + calcularLongitud(radio));
+
+	}
+
+	private static double calcularArea(double radio) {
+		return Math.PI * Math.pow(radio, 2);
+	}
+
+	private static double calcularLongitud(double radio) {
+		return 2 * Math.PI * radio;
+	}
+
+}
