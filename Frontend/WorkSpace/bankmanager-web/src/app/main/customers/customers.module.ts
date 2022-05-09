63286a29ef7df12d4835cb2fa7d662704269518e
Mario Cascudo <mariocscdclase@gmail.com>
2022-05-09 11:05:08 +0200

    Seguimos con el frontend

---
+++ Frontend/WorkSpace/bankmanager-web/src/app/main/customers/customers.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OntimizeWebModule } from 'ontimize-web-ngx';
+import { CustomersRoutingModule } from './customers-routing.module';
+import { CustomersHomeComponent } from './customers-home/customers-home.component';
+import { CustomersDetailComponent } from './customers-detail/customers-detail.component';
+
+
+@NgModule({
+  declarations: [CustomersHomeComponent, CustomersDetailComponent],
+  imports: [
+    CommonModule,
+    OntimizeWebModule,
+    CustomersRoutingModule
+  ]
+})
+export class CustomersModule { }
